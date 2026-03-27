1f8b89a6f6b961ca8e5f30845161e35e828979d9
Mohammed Omran <mohammedomran9512@gmail.com>
2026-03-27 07:40:33 +0200

    add main packages : mapster, mediatr, carter

---
+++ src/Catalog/Catalog.Api/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.Api.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public List<string> Category { get; set; } = new();
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
